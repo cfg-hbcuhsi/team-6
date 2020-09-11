89fd0901ea11f0191af2764dc60214c44565bb50
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2020-09-11 19:19:30 -0400

    Modify forms and buttons to mentorship opportunities

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -8,51 +8,51 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Paper from "@material-ui/core/Paper";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
 
 const useStyles = makeStyles((theme) => ({
     margin: {
         margin: theme.spacing(1),
     },
+    padding: {}
 }));
 
 export default function InputWithIcon() {
     const classes = useStyles();
 
+    const [value, setValue] = React.useState('Mentor');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+
     return (
         <div>
-            <Paper style={{padding: 10, width:200}}>
-                <FormControl className={classes.margin}>
-                    <InputLabel htmlFor="input-with-icon-adornment">With a start adornment</InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <AccountCircle/>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-                <TextField
-                    className={classes.margin}
-                    id="input-with-icon-textfield"
-                    label="TextField"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <AccountCircle/>
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-                <div className={classes.margin}>
-                    <Grid container spacing={1} alignItems="flex-end">
-                        <Grid item>
-                            <AccountCircle/>
-                        </Grid>
-                        <Grid item>
-                            <TextField id="input-with-icon-grid" label="With a grid"/>
-                        </Grid>
-                    </Grid>
+            <Paper style={{padding: 10, width: 400}}>
+                <div>
+                    <FormControl className={classes.margin}>
+                        <InputLabel>Name</InputLabel>
+                        <Input
+                            id="input-with-icon-adornment"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <AccountCircle/>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </div>
+                <div>
+                    <FormControl component="fieldset">
+                        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                            <FormControlLabel value="mentor" control={<Radio/>} label="I want to be a Mentor"/>
+                            <FormControlLabel value="mentee" control={<Radio/>} label="I want to be a Mentee"/>
+                        </RadioGroup>
+                    </FormControl>
                 </div>
             </Paper>
         </div>
