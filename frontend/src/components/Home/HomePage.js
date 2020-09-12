612953b07ac22cf2bd95f565e5cda3b5d51d0540
EugeneCharron <40960580+EugeneCharron@users.noreply.github.com>
2020-09-11 20:55:23 -0400

    major frontend changes
    added cards to front end!

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -2,56 +2,76 @@ import React from 'react';
 import { Grid, Grow, Typography } from '@material-ui/core';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import { Link, BrowserRouter } from 'react-router-dom';
-import useStyles from './styles';
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link'
 import { AnimationWrapper } from 'react-hover-animation';
-import CountUp from 'react-countup';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import useStyles from './styles.js';
+
 
 const infoCards = [
-  { color: '#00838f', title: 'Software Engineer', text: '', idx: 0 },
-  { color: '#1565c0', title: 'IT Support', info: '', text: '', idx: 1 },
-  { color: '#6495ED', title: 'Data Scientist', info: '', text: '', idx: 2},
+  { color: '#00838f', title: 'Law', text: '', idx: 0, link: '/Home' },
+  { color: '#1565c0', title: 'Medicine', info: '', text: '', idx: 1, link: '/CardDetails' },
+  { color: '#6495ED', title: 'Technology', info: '', text: '', idx: 2, link: '/CardDetails'},
+  { color: '#283593', title: 'Finance', info: '', text: '', idx: 3, link: '/CardDetails' },
+  { color: '#00838f', title: 'Education', text: '', idx: 4, link: '/CardDetails'},
+  { color: '#1565c0', title: 'Business', info: '', text: '', idx: 5, link: '/CardDetails'},
+  { color: '#6495ED', title: 'Trade Work', info: '', text: '', idx: 6, link: '/CardDetails'},
+  { color: '#283593', title: 'Engineering', info: '', text: '', idx: 7, link: '/CardDetails'},
 ];
 
-let arr = ['https://resize.hswstatic.com/w_1024/gif/becoming-doctor.jpg', 
-'https://www.thebalancecareers.com/thmb/5PUQ3eYS60TpLsOdjNYmY4kxY6U=/1500x844/smart/filters:no_upscale()/nurses-discussing-over-documents-in-hospital-493216353-595690893df78c4eb647536e-5bd78e3446e0fb0051debf7f.jpg', 
-'https://d2ueix13hy5h3i.cloudfront.net/wp-content/uploads/2019/12/inteligencia-artificial-e-ciencia-de-dados-para-executivos-data-science-1140x641-1024x576.jpg', 
+let arr = ['https://cdn.britannica.com/51/190451-050-0E9B50F5/soundblock-Wood-scales-books-stack-background-leather.jpg', 
+'https://img.freepik.com/free-vector/abstract-health-medical-science-healthcare-icon_36402-277.jpg?size=626&ext=jpg', 
+'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', 
+'https://cdn.hipwallpaper.com/i/87/59/yaQMpF.jpg',
+'https://giveme-5.org/wp-content/uploads/2019/11/modern-education-system.jpg',
+'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+'https://mk0pathwaytoausoq5ql.kinstacdn.com/wp-content/uploads/2017/10/pexels-photo-175039-1080x675.jpeg',
+'https://wallpapercave.com/wp/wp2700096.jpg'
 ]
 
-let salary = [80000, 45000, 78000]
-let majors = ['Computer Science', 'Information Technology', 'Computer Science']
-let employmentGrowth = [22, 15, 18]
-let yearsToCompleteSchool = [4, '2 - 4', 4]
 
-const CardDetails = () => {
+
+const NewsCards = () => {
   const classes = useStyles();
 
+
+  
     return (
+      <div>
+        <AppBar position="relative">
+        <Toolbar>
+          <Typography className={classes.mainTitle} color="inherit" noWrap>
+            Eat. Learn. Play.
+          </Typography>
+          <Button component={RouterLink} to='/CardDetails'>
+            Learn.
+          </Button>
+        </Toolbar>
+      </AppBar>
+
       <Grow in>
         <Grid className={classes.container} container alignItems="stretch" spacing={9}>
           {infoCards.map((infoCard) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard} >
+            <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
               <AnimationWrapper>
-              <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
-                <CardActionArea >
-                  <Typography className={classes.tex}>
+              <Typography className={classes.tex} color="inherit">
+                    
                     {infoCard.title}
+                    
                   </Typography>
-                  <CardMedia className={classes.pic} image={arr[infoCard.idx]}/>
-                  <Typography variant="h6" component="h6">Starting Salary: <br /> <i>{infoCard.text}</i></Typography>
-                  <Typography className= {classes.sal}>
-                    $<CountUp start={0} end={salary[infoCard.idx]} duration={2.75} separator={","}  />
-                  </Typography>
-                  <Typography>Employment Growth:<br /> <i>{infoCard.text}</i></Typography>
-                    <Typography className= {classes.growth}>
-                      <CountUp start={0} end={employmentGrowth[infoCard.idx]} duration={8}/>%
-                    </Typography>
-                  <Typography>Years to Complete School:<br /> <i>{infoCard.text}</i></Typography>
-                    <Typography className= {classes.years}>{yearsToCompleteSchool[infoCard.idx]}</Typography>
-                    <Typography>Popular College Majors: <br /> <i>{infoCard.text}</i></Typography>
-                    <Typography className= {classes.major}>{majors[infoCard.idx]}</Typography>
+              <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+              <Link underline='none' component={RouterLink} to={infoCard.link}>
+                
+                <CardActionArea>
+                
+                  <CardMedia className={classes.pic} component="img" alt="contemplative reptile" height="140" image={arr[infoCard.idx]} title="contemplative reptile"/>
+            
+                  
                 </CardActionArea>
-               
+                </Link>
               </div>
               </AnimationWrapper>
             </Grid>
@@ -59,7 +79,19 @@ const CardDetails = () => {
           ))}
         </Grid>
       </Grow>
+      <footer className={classes.footer}>
+      <Typography variant="h6" align="center" gutterBottom>
+        Footer
+      </Typography>
+      <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        Something here to give the footer a purpose!
+      </Typography>
+    </footer>
+    </div>
     );
   }
 
-export default CardDetails;
+  
+
+
+export default NewsCards;
--- frontend/src/components/Home/styles.js
+++ frontend/src/components/Home/styles.js
@@ -2,60 +2,54 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
   card: {
-    
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '110%',
-    height: '78vh',
-    padding: '0%',
-    borderRadius: 10,
-    color: 'white',
-    position: 'relative',
-    backgroundColor: 'red'
-
-  
+    display: 'flex', flexDirection: 'column', textAlign: 'center', width: '100%', justifyContent: 'center', position: 'relative'
+    // display: 'flex',
+    // flexDirection: 'column',
+    // justifyContent: 'space-between',
+    // alignItems: 'center',
+    // width: '100%',
+    // height: '30vh',
+    // padding: '0%',
+    // borderRadius: 10,
+    // color: 'white',
+    // position: 'relative',
+    // top: '100px',
   },
   infoCard: {
-    display: 'flex', flexDirection: 'column', textAlign: 'center', width: '100%', justifyContent: 'center', 
+    // display: 'flex', flexDirection: 'column', textAlign: 'center',
+    display: 'flex', flexDirection: 'column', textAlign: 'center', width: '100%', justifyContent: 'center', position: 'relative'
     
   },
   container: {
-    padding: '0% 0% 0% 0%', width: '100%', height: '100%', margin: 0,  justifyContent: 'center', 
+    padding: '0 5%', width: '100%', margin: 0, padding: 100, position: 'relative'
     
   },
   pic: {
-    position: 'relative',
-    height: '20vh',
+    height: '30vh',
     borderRadius: 10,
-    
+    position: 'relative'
     
   },
   tex: {
-    
+    display: 'flex',
     position: 'absolute',
-    fontFamily: 'arial',
-    backgroundColor: 'black',
-    borderRadius: 10,
-    left: '0px',
-    top: '0px',
-    fontSize: '30px',
-    color: '#00bcd4',
+    fontFamily: 'helvetica',
+    alignItems: 'left',
+    justifyContent: 'top',
+    bottom: '230px',
+    // left: '20px',
+    fontSize: '40px',
+    color: 'whitesmoke',
     
   },
-  sal: {
-    fontSize: '50px',
-  },
-  major: {
-    fontSize: '30px'
-  },
-  growth: {
-    fontSize: '50px'
-  },
-  years: {
-    fontSize: '60px'
-  }
-
   
+  mainTitle: {
+    align: "center",
+  },
+
+  footer: {
+    backgroundColor: 'dark blue',
+    padding: 100,
+    // background: linear-gradient('-45deg', '#ee7752', '#e73c7e', '#23a6d5', '#23d5ab'),
+  }
 });
