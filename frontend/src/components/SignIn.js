c790395bbc067ea51509ae81574e86c80ca7ecfe
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2020-09-11 21:27:14 -0400

    Make fields required and submittable
    This reverts commit 5e9f05d6888138ad90d6e85ba7cf5965d7cdca79.

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,71 +1,139 @@
-import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Paper from "@material-ui/core/Paper";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import EmailIcon from '@material-ui/icons/Email';
 
-const useStyles = makeStyles((theme) => ({
-    margin: {
-        margin: theme.spacing(1),
-    },
-    padding: {}
-}));
+import React, {Component} from 'react';
+import axios from 'axios';
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import {Grid} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
-export default function InputWithIcon() {
-    const classes = useStyles();
 
-    const [value, setValue] = React.useState('Mentor');
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
+        this.state = {
+            firstName: "",
+            lastName: '',
+            username: '',
+            email: "",
+            password: ""
+        };
+    }
+
+    handleInputChange = e => {
+        console.log(this.state)
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
     };
 
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state)
 
-    return (
-        <div>
-            <Paper style={{padding: 10, width: 300}}>
-                <div>
-                    <FormControl className={classes.margin}>
-                        <InputLabel>Name</InputLabel>
-                        <Input
+        const {firstName, lastName, username, email, password} = this.state;
+        const formInfo = {
+            firstName, lastName, username, email, password
+        };
+
+        // axios
+        //     .post('http://localhost:3001/create', formInfo)
+        //     .then(() => console.log('User created!'))
+        //     .catch(console.log);
+    };
+
+    render() {
+        return (
+            <Paper style={{width: 250, padding: 10}}>
+                <br/>
+                <div className="container">
+                    <form onSubmit={this.handleSubmit}>
+
+                        <TextField
+                            required label={"Email"}
+                            name={"email"}
+                            value={this.state.email}
+                            type={"text"}
+                            onChange={this.handleInputChange}
                             id="input-with-icon-adornment"
                             startAdornment={
                                 <InputAdornment position="start">
-                                    <AccountCircle/>
                                 </InputAdornment>
                             }
                         />
-                    </FormControl>
-                </div>
-                <div>
-                    <FormControl className={classes.margin}>
-                        <InputLabel>Email</InputLabel>
-                        <Input
+
+                        <TextField
+                            required
+                            label={"Password"}
+                            name={"password"}
+                            value={this.state.password}
+                            type={"text"}
+                            onChange={this.handleInputChange}
                             id="input-with-icon-adornment"
                             startAdornment={
                                 <InputAdornment position="start">
-                                    <EmailIcon/>
                                 </InputAdornment>
                             }
                         />
-                    </FormControl>
-                </div>
-                <div>
-                    <FormControl component="fieldset">
-                        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-                            <FormControlLabel value="mentor" control={<Radio/>} label="I want to be a Mentor"/>
-                            <FormControlLabel value="mentee" control={<Radio/>} label="I want to be a Mentee"/>
-                        </RadioGroup>
-                    </FormControl>
+
+                        <TextField
+                            required
+                            label={"First Name"}
+                            name={"firstName"}
+                            value={this.state.firstName}
+                            type={"text"}
+                            onChange={this.handleInputChange}
+                            id="input-with-icon-adornment"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                </InputAdornment>
+                            }
+                        />
+                        <TextField
+                            required
+                            label={"Last Name"}
+                            name={"lastName"}
+                            value={this.state.lastName}
+                            type={"text"}
+                            onChange={this.handleInputChange}
+                            id="input-with-icon-adornment"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                </InputAdornment>
+                            }
+                        />
+
+
+                        {/*<div>*/}
+                        {/*    <FormControl component="fieldset">*/}
+                        {/*        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={this.handleInputChange}>*/}
+                        {/*            <FormControlLabel value="mentor" control={<Radio/>} label="I want to be a Mentor"/>*/}
+                        {/*            <FormControlLabel value="mentee" control={<Radio/>} label="I want to be a Mentee"/>*/}
+                        {/*        </RadioGroup>*/}
+                        {/*    </FormControl>*/}
+                        {/*</div>*/}
+
+                        <Grid container justify="center" style={{marginTop: '10px'}}>
+                            <Button variant="outlined" color="primary" style={{textTransform: "none"}}
+                                    type={"submit"}>Sign Up</Button>
+                        </Grid>
+                    </form>
                 </div>
             </Paper>
-        </div>
-    );
+        );
+    }
 }
+
+
+export default SignIn;
+
