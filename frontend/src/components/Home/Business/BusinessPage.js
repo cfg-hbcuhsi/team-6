e3a46e448acab57d512c2a258e7b9dabe4ab27fb
Omar Azad <68172750+omarazad@users.noreply.github.com>
2020-09-12 11:43:35 -0400

    Business, Medicine, and Technology Page Finished

--- frontend/src/components/Home/Business/BusinessPage.js
+++ frontend/src/components/Home/Business/BusinessPage.js
@@ -5,6 +5,8 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import useStyles from './styles';
 import { AnimationWrapper } from 'react-hover-animation';
 import CountUp from 'react-countup';
+import Box from '@material-ui/core/Box';
+import { shadows } from '@material-ui/system';
 
 const infoCards = [
   { color: '#2F8DFF', title: 'Management Consultant', text: '', idx: 0 },
@@ -22,46 +24,57 @@ let majors = ['Management Consultant', 'Operations Analysit', 'Business Analyst'
 let employmentGrowth = [14, 26, 14]
 let yearsToCompleteSchool = [4, '4-6', 4]
 
+
 const CardDetails = () => {
   const classes = useStyles();
 
-    return (
-        <Grow in>
-        <Grid className={classes.container} container alignItems="stretch" spacing={9}>
-          {infoCards.map((infoCard) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard} >
-              <AnimationWrapper>
-              <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
-                <CardActionArea >
-                  <Typography className={classes.tex}>
-                    {infoCard.title}
-                  </Typography>
-                  <CardMedia className={classes.pic} image={arr[infoCard.idx]}/>
-
-                  <Typography> Career:<br /> <i>{infoCard.text}</i></Typography>
-                    <Typography className= {classes.majors}>{majors[infoCard.idx]}</Typography>
-
-                  <Typography variant="h7" component="h7"> Median Salary: <br /> <i>{infoCard.text}</i></Typography>
-                  <Typography className= {classes.sal}>
-                    $<CountUp start={0} end={salary[infoCard.idx]} duration={2.75} separator={","}  />
-                  </Typography>
-                  <Typography>Employment Growth:<br /> <i>{infoCard.text}</i></Typography>
-                    <Typography className= {classes.growth}>
-                      <CountUp start={0} end={employmentGrowth[infoCard.idx]} duration={8}/>%
-                    </Typography>
-                  <Typography>Years to Complete School:<br /> <i>{infoCard.text}</i></Typography>
-                    <Typography className= {classes.years}>{yearsToCompleteSchool[infoCard.idx]}</Typography>
-
-                </CardActionArea>
-               
-              </div>
-              </AnimationWrapper>
-            </Grid>
+  return (
+    <Grow in>
+      
+      <Grid className={classes.container} container alignItems="stretch" spacing={9}>
+      
+      
+        {infoCards.map((infoCard) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard} >
+            <Box boxShadow={3}
+      style={{ width: 'flex', height: 'flex' }}>
+            <AnimationWrapper>
             
-          ))}
-        </Grid>
-      </Grow>
-    );
-  }
+            <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+              <CardActionArea >
+                <Typography className={classes.tex}>
+                  {infoCard.title}
+                </Typography>
+                <CardMedia className={classes.pic} image={arr[infoCard.idx]}/>
+
+                <Typography> Career:<br /> <i>{infoCard.text}</i></Typography>
+                  <Typography className= {classes.majors}>{majors[infoCard.idx]}</Typography>
+
+                <Typography variant="h7" component="h7"> Entry Level Salary: <br /> <i>{infoCard.text}</i></Typography>
+                <Typography className= {classes.sal}>
+                  $<CountUp start={0} end={salary[infoCard.idx]} duration={2.75} separator={","}  />
+                </Typography>
+                <Typography>Employment Growth:<br /> <i>{infoCard.text}</i></Typography>
+                  <Typography className= {classes.growth}>
+                    <CountUp start={0} end={employmentGrowth[infoCard.idx]} duration={8}/>%
+                  </Typography>
+                <Typography>Years to Complete School:<br /> <i>{infoCard.text}</i></Typography>
+                  <Typography className= {classes.years}>{yearsToCompleteSchool[infoCard.idx]}</Typography>
+
+              </CardActionArea>
+             
+            </div>
+            
+            </AnimationWrapper>
+            </Box>
+          </Grid>
+          
+        ))}
+        
+      </Grid>
+      
+    </Grow>
+  );
+}
 
 export default CardDetails;
